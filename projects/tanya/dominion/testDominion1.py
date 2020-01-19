b422ff4cd65148f6b6f1a474b10b1d98594ee28b
tyl <tyl@tyl.com>
2020-01-19 16:35:09 +0800

    #nfr move play game and finish game to utility function

--- projects/tanya/dominion/testDominion1.py
+++ projects/tanya/dominion/testDominion1.py
@@ -28,44 +28,10 @@ supply = testUtility.pick10CardsToBeInSupply(box)
 #Update the supply have these cards
 testUtility.updateSupply(supply, player_names, nV,nC)
 
-#initialize the trash
-trash = []
-
 #Costruct the Player objects
 players = testUtility.ConstructPlayers(player_names)
 
-#Play the game
-turn  = 0
-while not Dominion.gameover(supply):
-    turn += 1    
-    print("\r")    
-    for value in supply_order:
-        print (value)
-        for stack in supply_order[value]:
-            if stack in supply:
-                print (stack, len(supply[stack]))
-    print("\r")
-    for player in players:
-        print (player.name,player.calcpoints())
-    print ("\rStart of turn " + str(turn))    
-    for player in players:
-        if not Dominion.gameover(supply):
-            print("\r")
-            player.turn(players,supply,trash)
-            
+testUtility.PlayGame(supply, supply_order, players)
 
 #Final score
-dcs=Dominion.cardsummaries(players)
-vp=dcs.loc['VICTORY POINTS']
-vpmax=vp.max()
-winners=[]
-for i in vp.index:
-    if vp.loc[i]==vpmax:
-        winners.append(i)
-if len(winners)>1:
-    winstring= ' and '.join(winners) + ' win!'
-else:
-    winstring = ' '.join([winners[0],'wins!'])
-
-print("\nGAME OVER!!!\n"+winstring+"\n")
-print(dcs)
+testUtility.FinishGame(players)
--- projects/tanya/dominion/testUtility.py
+++ projects/tanya/dominion/testUtility.py
@@ -87,3 +87,40 @@ def ConstructPlayers(player_names):
             players.append(Dominion.Player(name))
     return players
 
+def PlayGame(supply, supply_order, players):
+    #initialize the trash
+    trash = []
+    turn  = 0
+    #Play the game
+    while not Dominion.gameover(supply):
+        turn += 1    
+        print("\r")    
+        for value in supply_order:
+            print (value)
+            for stack in supply_order[value]:
+                if stack in supply:
+                    print (stack, len(supply[stack]))
+        print("\r")
+        for player in players:
+            print (player.name,player.calcpoints())
+        print ("\rStart of turn " + str(turn))    
+        for player in players:
+            if not Dominion.gameover(supply):
+                print("\r")
+                player.turn(players,supply,trash)
+
+def FinishGame(players):
+    dcs=Dominion.cardsummaries(players)
+    vp=dcs.loc['VICTORY POINTS']
+    vpmax=vp.max()
+    winners=[]
+    for i in vp.index:
+        if vp.loc[i]==vpmax:
+            winners.append(i)
+    if len(winners)>1:
+        winstring= ' and '.join(winners) + ' win!'
+    else:
+        winstring = ' '.join([winners[0],'wins!'])
+
+    print("\nGAME OVER!!!\n"+winstring+"\n")
+    print(dcs)
